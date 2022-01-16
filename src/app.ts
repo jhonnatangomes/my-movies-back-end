40a9cea5af8294a0d9ff1f467a02cb31ab3e00c4
Jhonnatan Gomes <jhonnatangomes05@gmail.com>
2022-01-16 15:07:19 -0300

    added route creation script

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,13 @@ import 'reflect-metadata';
 
 import connectDatabase from './database';
 
+import { setupRoute } from './config/route';
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+setupRoute(app);
 
 export async function init() {
     await connectDatabase();
---
+++ src/config/route.ts
@@ -0,0 +1,23 @@
+import { Router, Express } from 'express';
+import fg from 'fast-glob';
+
+const ROUTES_DIR =
+    process.env.NODE_ENV === 'production'
+        ? '**/routes/**Route.js'
+        : '**/src/routes/**Route.ts';
+
+export const setupRoute = (app: Express): void => {
+    const router = Router();
+    app.use(`/`, router);
+    fg.sync(ROUTES_DIR).map(async (routeFile) => {
+        const route = (await import(`../../${routeFile}`)).default;
+        route(router);
+        console.log(
+            'All routes: ',
+            router.stack.map((r) => ({
+                path: `/${r.route.path}`,
+                method: r.route.methods,
+            }))
+        );
+    });
+};
